2888faeb5bf6cbc39b71f09bc93265755fa56ca6
Szymon Wygnanski <s@finalclass.net>
2018-06-07 22:09:14 +0200

    update router

--- lib/forus_web/router.ex
+++ lib/forus_web/router.ex
@@ -9,18 +9,16 @@ defmodule ForusWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", ForusWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", HomeController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ForusWeb do
-  #   pipe_through :api
-  # end
+  scope "/accounts", ForusWeb.Accounts, as: :accounts do
+    pipe_through :browser
+    
+    resources "/users", UserController
+  end
+
 end
