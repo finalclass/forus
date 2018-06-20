f745d3ef63684b8fa61a904d4032b5ae26931943
Szymon Wygnanski <s@finalclass.net>
2018-06-20 09:36:43 +0200

    migration to buma

--- lib/forus_web/views/forms_helpers.ex
+++ lib/forus_web/views/forms_helpers.ex
@@ -1,24 +1,33 @@
 defmodule ForusWeb.FormsHelpers do
   use Phoenix.HTML
-  
-  def forus_form_field(f, field, [do: inner]) do
-    content_tag(:div, [
-          label(f, field),
-          inner,
-          ForusWeb.ErrorHelpers.error_tag(f, field),
-        ],
-      class: "form-group")
+
+  def forus_form_field(f, field, do: inner) do
+    content_tag(
+      :div,
+      [
+        label(f, field, class: 'label'),
+        content_tag(
+          :div,
+          [
+            inner
+          ],
+          class: 'control'
+        ),
+        ForusWeb.ErrorHelpers.error_tag(f, field)
+      ],
+      class: "field"
+    )
   end
-  
+
   def forus_text_field(f, field) do
     forus_form_field f, field do
-      text_input f, field, class: "form-control"
+      text_input(f, field, class: "input")
     end
   end
 
   def forus_password_field(f, field) do
     forus_form_field f, field do
-      password_input(f, field, class: "form-control")
+      password_input(f, field, class: "input")
     end
   end
 end
